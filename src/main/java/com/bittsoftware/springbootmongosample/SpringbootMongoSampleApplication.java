b8253d62c2f85fc1d93e2f1b4e9b1e15b28f1e22
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2023-09-16 15:01:12 -0300

    Project created

---
+++ src/main/java/com/bittsoftware/springbootmongosample/SpringbootMongoSampleApplication.java
@@ -0,0 +1,13 @@
+package com.bittsoftware.springbootmongosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bittsoftware/springbootmongosample/SpringbootMongoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bittsoftware.springbootmongosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
