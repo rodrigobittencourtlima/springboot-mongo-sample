92dcb5abec7e6f8b9954165772771c1b229e5869
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2023-09-18 14:21:33 -0300

    Users seeding

---
+++ src/main/java/com/bittsoftware/springbootmongosample/config/TestConfig.java
@@ -0,0 +1,30 @@
+package com.bittsoftware.springbootmongosample.config;
+
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.bittsoftware.springbootmongosample.models.entities.User;
+import com.bittsoftware.springbootmongosample.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostConstruct
+	public void init() {
+		userRepository.deleteAll();
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
