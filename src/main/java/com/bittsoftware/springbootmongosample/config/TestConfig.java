b4269703b3718d5eb52f8c3582ffb21d8db301a8
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2023-09-18 14:36:12 -0300

    Author, Comment, Post entities

--- src/main/java/com/bittsoftware/springbootmongosample/config/TestConfig.java
+++ src/main/java/com/bittsoftware/springbootmongosample/config/TestConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.bittsoftware.springbootmongosample.models.entities.User;
+import com.bittsoftware.springbootmongosample.repositories.PostRepository;
 import com.bittsoftware.springbootmongosample.repositories.UserRepository;
 
 @Configuration
@@ -18,9 +19,14 @@ public class TestConfig {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private PostRepository postRepository;
+
 	@PostConstruct
 	public void init() {
 		userRepository.deleteAll();
+		postRepository.deleteAll();
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
