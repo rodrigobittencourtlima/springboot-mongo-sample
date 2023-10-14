3f7f72a3b9ba7f4aa44167f67cf409afac5412e3
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2023-10-14 15:56:02 -0300

    POST user

--- src/main/java/com/bittsoftware/springbootmongosample/resource/UserResource.java
+++ src/main/java/com/bittsoftware/springbootmongosample/resource/UserResource.java
@@ -1,13 +1,17 @@
 package com.bittsoftware.springbootmongosample.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bittsoftware.springbootmongosample.models.dto.UserDTO;
 import com.bittsoftware.springbootmongosample.services.UserService;
@@ -27,6 +31,13 @@ public class UserResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		return ResponseEntity.ok().body(service.findById(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
